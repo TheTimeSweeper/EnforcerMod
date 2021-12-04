ae33ab9b7836edb8d05b25b6e44cff8e41cc212b
TimeSweeper <leo.midget@gmail.com>
2021-12-03 18:24:12 -0800

    fuck you nebby I did it anyways

---
+++ EnforcerMod_VS/TKNemforcer/EpicIntegrationPlugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using R2API.Utils;
+
+namespace TKNemforcer
+{
+
+    [R2APISubmoduleDependency(new string[] 
+    {
+        "LanguageAPI",
+    })]
+
+    [BepInDependency("com.TeamMoonstorm.TKStarstorm2", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(EnforcerPlugin.EnforcerModPlugin.MODUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(MODUID, "Add TK Nemesis", "6.9.0")]
+    public class EpicIntegrationPlugin : BaseUnityPlugin
+    {
+        public const string MODUID = "com.EnforcerGang.AddTKNemforcer";
+
+        void Awake()
+        {
+            //shartstorm 3 xDDDD
+            if (!BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.TeamMoonstorm.TKStarstorm2"))
+                return;
+
+            Logger.LogWarning("doin it doin it doin it now");
+
+            NemforcerStarstorm.Init();
+        }
+    }
+}
+
+
---
+++ EnforcerMod_VS/TKNemforcer/NemforcerInvasion.cs
@@ -0,0 +1,20 @@
+﻿using EntityStates.Events;
+
+namespace TKNemforcer
+{
+    public class NemforcerInvasion : GenericNemesisEvent
+    {
+        public override void OnEnter()
+        {
+            spawnCard = NemforcerStarstorm.NemforcerSpawnCard;
+            spawnDistanceString = "Close";
+            eventCard = NemforcerStarstorm.NemforcerInvasionCard;
+            drizzleDuration = 0;
+            typhoonDuration = 0;
+            warningDuration = 15;
+            base.OnEnter();
+        }
+    }
+}
+
+
