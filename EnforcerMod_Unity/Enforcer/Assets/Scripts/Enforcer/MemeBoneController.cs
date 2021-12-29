ecafd0e3ad4371faea1efc71a9bde723f06224b7
TimeSweeper <leo.midget@gmail.com>
2021-12-29 04:53:38 -0800

    it
    lives

---
+++ EnforcerMod_Unity/Enforcer/Assets/Scripts/Enforcer/MemeBoneController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MemeBoneController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform origBone;
+    public Transform OrigBone { get => origBone; set => origBone = value; }
+
+    void Update()
+    {
+        if (!origBone) return;
+        origBone.transform.rotation = transform.rotation;
+        origBone.transform.position = transform.position;
+    }
+}
---
+++ EnforcerMod_Unity/Enforcer/Assets/Scripts/Enforcer/MemeRigController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MemeRigController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator memeAnimator;
+
+    [SerializeField]
+    private Animator origAnimator;
+
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void playMemeAnim(string anim)
+    {
+        origAnimator.enabled = false;
+        gameObject.SetActive(true);
+        memeAnimator.Play(anim);
+    }
+
+    [ContextMenu("testStopAnim")]
+    public void stopAnim()
+    {
+        memeAnimator.Play("Empty");
+        gameObject.SetActive(false);
+        origAnimator.enabled = true;
+    }
+
+    [ContextMenu("testPlayAnim")]
+    public void test()
+    {
+        playMemeAnim("def");
+    }
+    [ContextMenu("testPlayAnim2")]
+    public void test2()
+    {
+        playMemeAnim("EnforcerOfficerEarlRun");
+    }
+
+#if UNITY_EDITOR
+
+    //blasphemy declaring a variable oustide of the top?
+    [SerializeField]
+    private Transform origArmature;
+
+    [ContextMenu("setBones")]
+    public void SetBones()
+    {
+        if (origAnimator == null)
+            return;
+
+        List<MemeBoneController> oldMemeBones = GetComponentsInChildren<MemeBoneController>(true).ToList();
+
+        for (int i = 0; i < oldMemeBones.Count; i++)
+        {
+            UnityEditor.Undo.RecordObject(oldMemeBones[i], "adding meme bone");
+            DestroyImmediate(oldMemeBones[i]);
+        }
+
+        List<Transform> memeAnimBones = GetComponentsInChildren<Transform>(true).ToList();
+        List<Transform> origBones = origArmature.GetComponentsInChildren<Transform>(true).ToList();
+
+        int matchCount = 0;
+        for (int i = 0; i < memeAnimBones.Count; i++)
+        {
+            Transform memeBone = memeAnimBones[i];
+            Transform match = origBones.Find(tran => tran.name == memeBone.name);
+
+            if (match)
+            {
+                matchCount++;
+                UnityEditor.Undo.RecordObject(memeBone, "adding meme bone");
+                MemeBoneController boneController = memeBone.gameObject.AddComponent<MemeBoneController>();
+                boneController.OrigBone = match;
+            }
+        }
+        Debug.Log($"{matchCount} meme bones added!");
+    }
+#endif
+}
